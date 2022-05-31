fb674313319e5e081f78ac614ef427b6e6a66b6a
Matheus Silva <ms25022003@gmail.com>
2022-05-30 21:23:55 -0300

    aula-05

---
+++ aula-05/src/components/Input/index.jsx
@@ -0,0 +1,69 @@
+import React, { useContext, useState } from "react";
+import { SolicitacaoContext } from "../../context/contextoFormulario";
+import PropTypes from 'prop-types';
+
+const Input = ({ name, label, type = "text", refe}) => {
+  const [solicitacaoValue, setSolicitacaoValue] = useState("");
+
+  const { dispatch } = useContext(SolicitacaoContext)
+  // Aqui devemos acessar o estado global para obter os dados
+  // do formulário e uma maneira de atualizá-los.
+
+  /**
+   * Aqui pego o evento do input e atualizo o estado local do input em um useState
+   * @author Matheus Silva
+   * @param {EventListener} event - Evento do input 
+   * @returns {void}
+   */
+  const onChange = (e) => {
+    let {value} = e.target;
+    setSolicitacaoValue(value)
+  };
+  
+
+  /**
+   * Efetua o dispatch no context e atualiza o estado global
+   * @author Matheus Silva
+   * @param {EventListener} e - evento onBlur do input
+   * @returns
+   */
+  const onBlur = (e) => {
+    e.preventDefault();
+
+    // Aqui devemos atualizar o estado global com os dados de
+    // cada entrada.
+    // DICA: Podemos usar o nome de cada entrada para salvar
+    // os dados no estado global usando uma notação { chave: valor }
+    dispatch({
+      type: `ATUALIZAR_${refe}`,
+      payload: {
+        [name]: solicitacaoValue
+      }
+    })
+  };
+
+  return (
+    <div className="input-receptor">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        value={solicitacaoValue}
+        id={name}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+    </div>
+  );
+};
+
+Input.defaultProps = {
+  type: "text"
+}
+Input.PropTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  refe: PropTypes.string.isRequired
+}
+
+export default Input;
